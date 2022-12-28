2d2fe9c700d31ed3e71dc56a09fdbf2b3298e003
Raghu Saxena <poiasdpoiasd@live.com>
2022-12-28 13:59:53 +0800

    Make pair server event emitter, start and _listen

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import zmq from 'zeromq';
+import { PairingServer } from './lib/pairingServer.js';
 
 const keys = {
     publicKey: 'NrUa/J@7SEiev!<@y#bA::*2Uan:z+hV8dI&xcSX',
@@ -17,16 +18,25 @@ const sockNotif = new zmq.Pull({
 });
 
 const sockPair = new zmq.Reply();
+const sp2 = new PairingServer({
+    address: '0.0.0.0',
+    port: '31224',
+    notificationPort: '6969',
+    notificationPublicKey: keys.publicKey,
+});
 
 await sockNotif.bind("tcp://0.0.0.0:6969")
-await sockPair.bind("tcp://0.0.0.0:4444");
+// await sockPair.bind("tcp://0.0.0.0:4444");
 
-(async() => {
-    for await (const [msg1, msg2] of sockPair) {
-        console.log(msg1.toString(), msg2.toString());
-        await sockPair.send([Buffer.from("6969"), Buffer.from(keys.publicKey)]);
-    }
-})();
+await sp2.bind();
+sp2.start();
+
+// (async() => {
+//     for await (const [msg1, msg2] of sockPair) {
+//         console.log(msg1.toString(), msg2.toString());
+//         await sockPair.send([Buffer.from("6969"), Buffer.from(keys.publicKey)]);
+//     }
+// })();
 
 (async() => {
     while (true){
