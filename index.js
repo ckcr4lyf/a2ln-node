b68d71ef1290e0192d2f73a02f5044ee08a1398a
Raghu Saxena <poiasdpoiasd@live.com>
2022-12-31 14:36:29 +0800

    use only lib stuff in index

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import zmq from 'zeromq';
 import { PairingServer } from './lib/pairingServer.js';
+import { NotificationServer } from './lib/notificationServer.js';
 
 const keys = {
     publicKey: 'NrUa/J@7SEiev!<@y#bA::*2Uan:z+hV8dI&xcSX',
@@ -9,27 +9,35 @@ const keys = {
 // const zapHandler = new zmq.Reply();
 // await zapHandler.bind("inproc://zeromq.zap.01");
 
-const sockNotif = new zmq.Pull({
-    curveServer: true,
-    curveSecretKey: keys.secretKey,
-    curvePublicKey: keys.publicKey,
-    // zapDomain: "inproc://zeromq.zap.01",
-    // zapEnforceDomain: false,
+// const sockNotif = new zmq.Pull({
+//     curveServer: true,
+//     curveSecretKey: keys.secretKey,
+//     curvePublicKey: keys.publicKey,
+//     // zapDomain: "inproc://zeromq.zap.01",
+//     // zapEnforceDomain: false,
+// });
+
+const sn2 = new NotificationServer({
+    address: '0.0.0.0',
+    port: '6969',
+    keys: keys,
 });
 
-const sockPair = new zmq.Reply();
+// const sockPair = new zmq.Reply();
 const sp2 = new PairingServer({
     address: '0.0.0.0',
     port: '31224',
-    notificationPort: '6969',
-    notificationPublicKey: keys.publicKey,
+    notificationPort: sn2.options.port,
+    notificationPublicKey: sn2.options.keys.publicKey,
 });
 
-await sockNotif.bind("tcp://0.0.0.0:6969")
+// await sockNotif.bind("tcp://0.0.0.0:6969")
 // await sockPair.bind("tcp://0.0.0.0:4444");
 
 await sp2.bind();
+await sn2.bind();
 sp2.start();
+sn2.start();
 
 // (async() => {
 //     for await (const [msg1, msg2] of sockPair) {
@@ -38,13 +46,13 @@ sp2.start();
 //     }
 // })();
 
-(async() => {
-    while (true){
-        const msg = await sockNotif.receive();
-        if (msg.length === 4){
-            console.log(`[IMAGE] App: ${msg[0].toString()}, Title: ${msg[1].toString()}, Body: ${msg[2].toString()}`)
-        } else {
-            console.log(`App: ${msg[0].toString()}, Title: ${msg[1].toString()}, Body: ${msg[2].toString()}`)
-        }
-    }
-})();
+// (async() => {
+//     while (true){
+//         const msg = await sockNotif.receive();
+//         if (msg.length === 4){
+//             console.log(`[IMAGE] App: ${msg[0].toString()}, Title: ${msg[1].toString()}, Body: ${msg[2].toString()}`)
+//         } else {
+//             console.log(`App: ${msg[0].toString()}, Title: ${msg[1].toString()}, Body: ${msg[2].toString()}`)
+//         }
+//     }
+// })();
