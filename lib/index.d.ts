1b2906ac2d4db1d9ae95d34be03bf10ece82eb57
Raghu Saxena <poiasdpoiasd@live.com>
2023-01-16 13:54:30 +0800

    manually add types

---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+import { NotificationServer } from "./notificationServer.js";
+import { PairingServer } from "./pairingServer.js";
+export { NotificationServer, PairingServer };
---
+++ lib/pairingServer.d.ts
@@ -0,0 +1,58 @@
+/**
+ * @typedef PairingServerProperties
+ * @type {object}
+ * @property {string} address - the address to bind the server to
+ * @property {string} port - the port for the server to listen on
+ * @property {string} notificationPublicKey - public key of the notification server
+ * @property {string} notificationPort - port the notification server is listening on
+ */
+export class PairingServer extends EventEmitter {
+    /**
+     * @param {PairingServerProperties} options - Options for the pairing server
+     */
+    constructor(options: PairingServerProperties);
+    options: PairingServerProperties;
+    bound: boolean;
+    socket: any;
+    /**
+     * bind() binds the socket to the provided address & port and starts listening for connections
+     * @returns a promise that resolves when the bind operation is complete.
+     */
+    bind(): Promise<void>;
+    /**
+     * start() will begint o listen for messages on the socket for pairing requests
+     * and accept ANY/ALL speicication-valid pairing requests (NO CLIENT AUTH!!!)
+     *
+     * @returns void
+     */
+    start(): void;
+    _listen(): Promise<void>;
+    /**
+     * @param {'pair'} event
+     * @param {(pairData: { ip: string, publicKey: string }) => void} listener
+     * @returns {this}
+     */
+    on(event: 'pair', listener: (pairData: {
+        ip: string;
+        publicKey: string;
+    }) => void): this;
+}
+export type PairingServerProperties = {
+    /**
+     * - the address to bind the server to
+     */
+    address: string;
+    /**
+     * - the port for the server to listen on
+     */
+    port: string;
+    /**
+     * - public key of the notification server
+     */
+    notificationPublicKey: string;
+    /**
+     * - port the notification server is listening on
+     */
+    notificationPort: string;
+};
+import { EventEmitter } from "events";
